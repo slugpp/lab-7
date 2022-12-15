4b15a2fad9ec99eebf973ef356e27d91d6a1c9b3
fear <corefea@gmail.com>
2022-12-15 22:58:23 +0700

    lab7_3 completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,38 @@
 #include<iostream>
+#include<cmath>
 
 using namespace std;
+  int adiff(int a,int b)
+  { int k,y;
+    y=b-a;
 
+    if(y < 0)
+    {
+      k = abs(y);
+        if( k%360 <= 180 ){
+          return k%360;
+        }
+        else{ 
+            return 360 - k%360;
+        }
+    }
+     else
+     if( y%360 <= 180 ){
+          return y%360;
+        }
+        else{ 
+            return 360 - y%360;
+        }
+
+  }
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  cout << adiff(180,270)<< endl;
+  cout << adiff(210,45)<< endl;
+  cout << adiff(0,360)<< endl;
+  cout << adiff(10,350)<< endl;
+  cout << adiff(95,260)<< endl;
+  cout << adiff(90,-90)<< endl;
+  cout << adiff(1000,280)<< endl;
+  cout << adiff(60,244)<< endl;
 }
